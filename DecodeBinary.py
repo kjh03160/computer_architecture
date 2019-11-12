fc6629abcdcd4fdc5fc498ca3fd4cee77704b1d7
kjh03160 <49090888+kjh03160@users.noreply.github.com>
2019-11-12 11:03:04 +0900

    PC값 증가 오류 수정

--- DecodeBinary.py
+++ DecodeBinary.py
@@ -3,14 +3,14 @@ from ALU import ALU
 
 class DecodeBinary:
 
-    Opcode = [
+    Opcode_list = [
     ["R-format", "bltz", "j", "jal", "beq", "bne", "", ""],
     ["addi", "", "slti", "", "andi", "ori", "xori", "lui"],
     ["", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", ""],
     ["lb", "", "", "lw", "lbu", "", "", ""],
     ["sb", "", "", "sw", "", "", "", ""]]
-    Funct = [
+    Funct_list = [
     ["sll", "", "srl", "sra", "", "", "", ""],
     ["jr", "", "", "", "syscall", "", "", ""],
     ["mfhi", "", "mflo", "", "", "", "", ""],
@@ -23,16 +23,38 @@ class DecodeBinary:
         self.Simulator = Simulator
 
 
-    def decode_step(self, inst):
-        index = self.Simulator.Index - 1
-        inst = inst
-        # 바이너리 명령어 해석
-        # 해석 결과 값은 딕셔너리로 저장이 좋을 듯
-        # result = {'func' : 'add', 'rs' : '0xa', 'rd': ' 0xb', 'rt' : '0xc', 'shamt' : '0'}
+    def decode_step(self, index):
+        self.Simulator.PC = ALU.addSubstract(self.Simulator.PC, 4, 0)
+        inst = self.Simulator.InsMEM[index]
+        print(hex(inst))
+        opCode = inst >> 26
+        opCode_front = opCode >> 3
+        opCode_back = opCode & 7
+        if opCode == 0:
+            funct = inst & 63
+            funct_front = funct >> 3
+            funct_back = funct & 7
+            func = self.Opcode_list[funct_front][funct_back]
+            rs = (inst >> 21) & 31
+            rt = (inst >> 16) & 31
+            rd = (inst >> 11) & 31
+            sh = (inst >> 6) & 31
+            result = {'func': func, 'rs' : rs, 'rt' : rt, 'rd' : rd, 'shamt' : sh}
 
-        self.Simulator.decode(result)
-        ALU.addSubstract(self.Simulator.PC, 4, 0)
-        return index
+        else:
+            func = self.Opcode_list[opCode_front][opCode_back]
+            if 'j' in func:
+                j_add = (inst << 6) >> 6
+                result = {'func' : func, 'j_add' : j_add}
+            else:
+                offset = inst & 65535
+                rs = (inst >> 21) & 31
+                rt = (inst >> 16) & 31
+                result = {'func' : func, 'rs' : rs, 'rt' : rt, 'offset' : offset}
+        print(result)
+
+        self.Simulator.DecodeAssem.decode(result)
+        # return index
 
     def decode_All(self, InstMEM):
         for i in InstMEM:
