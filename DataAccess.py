e2ea4502fc637e130d5483fcd45abb5fb599cc7d
kjh03160 <49090888+kjh03160@users.noreply.github.com>
2019-11-12 13:33:51 +0900

    address 값 수정

--- DataAccess.py
+++ DataAccess.py
@@ -5,23 +5,23 @@ class DataAccess:
         self.Simulator = Simulator
 
     def MEM_LoadWord(self, address):
-        address -= 0x1000000
+        address -= 0x10000000
         return self.Simulator.DataMEM[address]
 
     def MEM_StoreWord(self, value, address):
-        address -= 0x1000000
+        address -= 0x10000000
         self.Simulator.DataMEM[address] = value
 
     def MEM_LoadByte(self, address):
-        address -= 0x1000000
+        address -= 0x10000000
         return self.Simulator.DataMEM[address] & 0xFF
 
     def MEM_LoadByteU(self, address):
-        address -= 0x1000000
+        address -= 0x10000000
         result = self.Simulator.DataMEM[address] << 24
         return ALU.ALU_main(24, result, 0b0010)
 
     def MEM_StoreByte(self, value, address):
-        address -= 0x1000000
+        address -= 0x10000000
         self.Simulator.DataMEM[address] = value & 0xFF
 
