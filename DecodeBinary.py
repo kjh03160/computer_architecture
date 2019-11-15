27d0d01d1b46f751858f42c7dcd927c389b85820
kjh03160 <49090888+kjh03160@users.noreply.github.com>
2019-11-15 22:56:12 +0900

    최종

--- DecodeBinary.py
+++ DecodeBinary.py
@@ -1,62 +1,98 @@
-from ALU import ALU
+class ALU:
+    Z = 0
 
+    @staticmethod
+    def ALU_main(x, y, c):
+        ret = None
+        c32 = (c >> 2) & 0x3
+        c10 = c & 0x3
+        if c32 == 0:
+            ret = ALU.shiftOperation(x, y, c10)
 
-class DecodeBinary:
+        elif c32 == 1:
+            ret = ALU.checkSetless(x, y)
 
-    Opcode_list = [
-    ["R-format", "bltz", "j", "jal", "beq", "bne", "", ""],
-    ["addi", "", "slti", "", "andi", "ori", "xori", "lui"],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["lb", "", "", "lw", "lbu", "", "", ""],
-    ["sb", "", "", "sw", "", "", "", ""]]
-    Funct_list = [
-    ["sll", "", "srl", "sra", "", "", "", ""],
-    ["jr", "", "", "", "syscall", "", "", ""],
-    ["mfhi", "", "mflo", "", "", "", "", ""],
-    ["mul", "", "", "", "", "", "", ""],
-    ["add", "", "sub", "", "and", "or", "xor", "nor"],
-    ["", "", "", "slt", "", "", "", ""]
-    ]
+        elif c32 == 2:
+            ret = ALU.addSubstract(x, y, c10)
+            S = ret
+            ALU.Z = ALU.checkZero(S)
 
-    def __init__(self, Simulator):
-        self.Simulator = Simulator
+        elif c32 == 3:
+            ret = ALU.logicOperation(x, y, c10)
 
+        return ret & 0xFFFFFFFF
 
-    def decode_step(self, index):
-        self.Simulator.PC = ALU.addSubstract(self.Simulator.PC, 4, 0)
-        inst = self.Simulator.InsMEM[index]
-        print(hex(inst))
-        opCode = inst >> 26
-        opCode_front = opCode >> 3
-        opCode_back = opCode & 7
-        if opCode == 0:
-            funct = inst & 63
-            funct_front = funct >> 3
-            funct_back = funct & 7
-            func = self.Opcode_list[funct_front][funct_back]
-            rs = (inst >> 21) & 31
-            rt = (inst >> 16) & 31
-            rd = (inst >> 11) & 31
-            sh = (inst >> 6) & 31
-            result = {'func': func, 'rs' : rs, 'rt' : rt, 'rd' : rd, 'shamt' : sh}
+    @staticmethod
+    def EndFunc():
+        print("None")
 
+    @staticmethod
+    def logicOperation(x, y, c10):
+        if c10 < 0 or c10 > 3:
+            ALU.EndFunc()
+
+        if c10 == 0:
+            return x & y
+        elif c10 == 1:
+            return x | y
+        elif c10 == 2:
+            return x ^ y
+        elif c10 == 3:
+            return ~(x | y)
+
+    @staticmethod
+    def shiftOperation(v, y, c):
+        ret = None
+        v = v & 0x001F # 5bit
+        if c < 0 or c > 3:
+            ALU.EndFunc()
+        if c == 0:
+            ret = y
+        elif c == 1:
+            y = y << v
+            ret = y
+        elif c == 3:
+            msb = y
+            msb = msb >> 31
+            if msb == 1:
+                y = y >> v
+                y = y | (0xFFFFFFFF << (32 - v))
+                ret = y
+                return ret & 0xFFFFFFFF
+            if msb == 0:
+                y = y >> v
+                ret = y
+                return ret & 0xFFFFFFFF
+        elif c == 2:
+            y = y >> v
+            ret = y
+
+        return ret & 0xFFFFFFFF
+
+    @staticmethod
+    def addSubstract(x, y, c10):
+        ret = None
+        if c10 < 0x0 or c10 > 0x3:
+            print("error")
+            ALU.EndFunc()
+        elif c10 == 0x0 or c10 == 0x2:
+            ret = x + y
+        elif c10 == 0x1 or c10 == 0x3:
+            ret = x - y
+
+        return ret & 0xFFFFFFFF
+
+    @staticmethod
+    def checkZero(s):
+
+        if s == 0:
+            return 1
         else:
-            func = self.Opcode_list[opCode_front][opCode_back]
-            if 'j' in func:
-                j_add = (inst << 6) >> 6
-                result = {'func' : func, 'j_add' : j_add}
-            else:
-                offset = inst & 65535
-                rs = (inst >> 21) & 31
-                rt = (inst >> 16) & 31
-                result = {'func' : func, 'rs' : rs, 'rt' : rt, 'offset' : offset}
-        print(result)
+            return 0
 
-        self.Simulator.DecodeAssem.decode(result)
-        # return index
-
-    def decode_All(self, InstMEM):
-        for i in InstMEM:
-            self.decode_step(i)
-
+    @staticmethod
+    def checkSetless(x, y):
+        if x < y:
+            return 1
+        else:
+            return 0
